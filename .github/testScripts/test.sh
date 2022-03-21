605c9a0733f69b77431e48850df98f6064660893
Bobby Espinoza <respinoza@ucar.edu>
2022-03-21 15:41:27 -0600

    Github Actions: docker push on successful git push or PR merge

--- .github/testScripts/test.sh
+++ .github/testScripts/test.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+# Wait and listen for RAMADDA to fire up
+nc -z -w300 127.0.0.1 8080
+for i in {1..5}; do curl -o /dev/null http://127.0.0.1:8080/repository/ && break || \
+(echo sleeping 15... && sleep 15); done
+
 curl -o ./.github/testScripts/actual.html http://127.0.0.1:8080/repository && \
 grep "Geode Systems" ./.github/testScripts/actual.html && \
 echo RAMADDA OK
